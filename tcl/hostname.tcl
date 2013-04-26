fc5af6bf9d80e5eaa41e9878fd7ccf5408266b93
David Osborne <david@qcode.co.uk>
2013-04-26 16:00:46 +0100

    hostname, ntp, postfix and locale generic procs

---
+++ tcl/hostname.tcl
@@ -0,0 +1,22 @@
+package provide muppet 1.0
+package require qcode 1.17
+namespace eval muppet {
+    namespace export *
+}
+
+proc hostname_update { hostname domain } {
+    #| update /etc/hosts and /etc/hostname so that hostname and hostname -f return the
+    # correct values (required for [my fqdn] etc.)
+
+    # /etc/hosts
+    set hosts {}
+    lappend hosts "127.0.0.1	localhost localhost.localdomain"
+    lappend hosts "127.0.1.1	${hostname}.${domain} $hostname"
+    lappend hosts ""
+    muppet::file_write /etc/hosts [join $hosts \n]
+    # /etc/hostname
+    muppet::file_write /etc/hostname "$hostname\n"
+    # run hostname command to update hostname for this session
+    sh hostname $hostname
+
+}
---
+++ tcl/ntp.tcl
@@ -0,0 +1,49 @@
+package provide muppet 1.0
+package require qcode 1.17
+namespace eval muppet {
+    namespace export *
+}
+
+proc ntp_generic_client_conf {} {
+    #| Generic NTP config which should be viable within a VM (tinker panic 0)
+    return {tinker panic 0
+driftfile /var/lib/ntp/ntp.drift
+
+# Enable this if you want statistics to be logged.
+#statsdir /var/log/ntpstats/
+
+statistics loopstats peerstats clockstats
+filegen loopstats file loopstats type day enable
+filegen peerstats file peerstats type day enable
+filegen clockstats file clockstats type day enable
+
+# You do need to talk to an NTP server or two (or three).
+#server ntp.your-provider.example
+
+server 0.uk.pool.ntp.org iburst
+server 1.uk.pool.ntp.org iburst
+server 2.uk.pool.ntp.org iburst
+server 3.uk.pool.ntp.org iburst
+
+# By default, exchange time with everybody, but don't allow configuration.
+restrict -4 default kod notrap nomodify nopeer noquery
+restrict -6 default kod notrap nomodify nopeer noquery
+
+# Local users may interrogate the ntp server more closely.
+restrict 127.0.0.1
+restrict ::1
+
+    }
+}
+
+proc ntp_generic_client_install {} {
+    # Install and start NTP client
+    
+    install ntp
+
+    # Write config files
+    file_write "/etc/ntp.conf" [ntp_generic_client_conf] 0644
+
+    # Restart daemon
+    service ntp restart
+}
