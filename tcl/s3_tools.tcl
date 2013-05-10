b86467dfdccd8cea132dc16ab48279fb0caa876f
David Osborne <david@qcode.co.uk>
2013-05-10 17:28:45 +0100

    EoD

--- tcl/s3_tools.tcl
+++ tcl/s3_tools.tcl
@@ -66,7 +66,15 @@ proc muppet::s3_get { bucket path } {
     set headers [s3_auth_headers GET $path $bucket] 
     puts "url = [s3_url $bucket]$path"
     set result [qc::http_get -headers $headers [s3_url $bucket]$path]
-    puts $result
+    return $result
+}
+
+proc muppet::s3_delete { bucket path } {
+    #| Construct the http DELETE request to S3 including auth headers
+    puts "path - $path bucket = $bucket"
+    set headers [s3_auth_headers DELETE $path $bucket] 
+    puts "url = [s3_url $bucket]$path"
+    set result [qc::http_delete -headers $headers [s3_url $bucket]$path]
     return $result
 }
 
@@ -86,14 +94,18 @@ proc muppet::s3_put { bucket filename path } {
     set content_md5 [::base64::encode [::md5::md5 -file $filename]]
     # Authentication value needs to use content_* values for hmac signing
     set headers [s3_auth_headers -content_type $content_type -content_md5 $content_md5 PUT $path $bucket] 
-    lappend headers Content-Length [file size $filename]
+    set file_size [file size $filename]
+    lappend headers Content-Length $file_size
     lappend headers Content-MD5 $content_md5
     lappend headers Content-Type $content_type
     # Stop tclcurl from stending Transfer-Encoding header
     lappend headers Transfer-Encoding {}
     puts "headers = $headers"
     puts "url = [s3_url $bucket]$path"
-    return [qc::http_put -headers $headers [s3_url $bucket]$path $filename]
+    # Have timeout values roughly in proportion to the filesize
+    # In this case allowing 100,000 bytes per second
+    set timeout [expr {$file_size/100000}]
+    return [qc::http_put -headers $headers -timeout $timeout [s3_url $bucket]$path $filename]
 }
 
 proc string2hex s {
@@ -104,11 +116,12 @@ proc string2hex s {
 proc muppet::s3 { args } {
     #| Access Amazon S3 buckets via REST API
     # Usage: s3 subcommand {args}
-    # where subcommand is one of ls, lsbucket, put
+    # where subcommand is one of ls, lsbucket, put, get or delete
     switch [lindex $args 0] {
         ls {
             # usage: s3 ls
-	    return [muppet::xml_nodes2text [muppet::s3_xml_nodes_select [s3_get "" /] {/ns:ListAllMyBucketsResult/ns:Buckets/ns:Bucket/ns:Name/text()} ]]
+            set nodes [muppet::s3_xml_select [s3_get "" /] {/ns:ListAllMyBucketsResult/ns:Buckets/ns:Bucket}]
+            return [qc::lapply muppet::s3_xml_node2dict $nodes]
         }
         lsbucket {
             # usage: s3 lsbucket bucket {prefix}
@@ -119,7 +132,7 @@ proc muppet::s3 { args } {
             } else {
                 set xmlDoc [s3_get [lindex $args 1] /]
             }
-	    return [muppet::xml_nodes2text [muppet::s3_xml_nodes_select $xmlDoc {/ns:ListBucketResult/ns:Contents/ns:Key} ]]
+	    return [qc::lapply muppet::s3_xml_node2dict [muppet::s3_xml_select $xmlDoc {/ns:ListBucketResult/ns:Contents}]]
         }
         get {
             # usage: s3 get bucket remote_path local_path
@@ -129,38 +142,43 @@ proc muppet::s3 { args } {
             # usage: s3 put bucket local_path remote_path
             s3_put {*}[lrange $args 1 end]
         }
+        delete {
+            # usage: s3 delete bucket remote_filename
+            s3_delete {*}[lrange $args 1 end]
+        }
         default {
-            error "Unknown s3 command. Must be one of ls, lsbucket or put."
+            error "Unknown s3 command. Must be one of ls, lsbucket, get or put."
         }
     }
 }
 
-proc muppet::s3_xml_select { xmlDoc xpath } {
-    #| Overly simplistic proc to return a list of values from a xmlDoc
-    # Could be extended to return more complex data structures like ldict or multimaps
-    # containing several tags
+proc muppet::s3_xml_select { xmlDoc xpath} {
+    #| Returns xml nodes specified by the supplied xpath.
+    # any namespace specified in the xmlns attribute is mapped to "ns" for use in the xpath query.
     set doc [dom parse $xmlDoc]
     set root [$doc documentElement]
-    $doc selectNodesNamespaces "ns [$root getAttribute xmlns]"
+    if { [$root hasAttribute xmlns] } {
+        $doc selectNodesNamespaces "ns [$root getAttribute xmlns]"
+    }
     return [$root selectNodes $xpath] 
 }
 
-proc muppet::s3_xml_nodes2ldict { node_list } {
-    set ldict [list]
-    foreach node $node_list {
-        set dict {}
-        foreach child [$node childNodes] {
-            dict set dict [$child nodeName] [$child asText]
+proc muppet::s3_xml_node2dict { node } {
+    #| Converts an XML tdom node into a dict.
+    # Use muppet::s3_xml_select to select suitable nodes with non-repeating elements
+    set dict ""
+    set nodes [$node childNodes]
+    foreach node $nodes {
+        if { [llength [$node childNodes]] > 1 \
+           || ([llength [$node childNodes]] == 1 \
+              && [ne [[$node firstChild] nodeType] TEXT_NODE] ) } {
+            lappend dict [$node nodeName] [muppet:::s3_xml_node2dict $node]
+        }  elseif { [llength [$node childNodes]] == 0 } {
+            # empty node
+            lappend dict [$node nodeName] {}
+        } else {
+            lappend dict [$node nodeName] [$node asText]
         }
-        lappend ldict $dict
     }
-    return $ldict
-}
-
-proc muppet::xml_nodes2text { list } {
-    set result {}
-    foreach node $list {
-        lappend result [$node asText]
-    }
-    return $result
+    return $dict
 }
