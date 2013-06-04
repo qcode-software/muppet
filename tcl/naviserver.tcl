fbd6c8dbacbbffe836391803c11ae8141c342485
David Osborne <david@qcode.co.uk>
2013-06-04 16:46:19 +0100

    Add naviserver defaults

--- tcl/naviserver.tcl
+++ tcl/naviserver.tcl
@@ -4,10 +4,6 @@ namespace eval muppet {
     namespace export *
 } 
 
-proc muppet::naviserver_install {} {
-    install naviserver-core naviserver-nsdbpg naviserver
-}
-
 proc muppet::naviserver_daemontools_run { service } {
     #| Naviserver start script which will fall-back to aolserver if Naviserver is not where expected
     return [subst -nocommands {#!/bin/sh
@@ -29,3 +25,36 @@ proc muppet::naviserver_service {service} {
     file delete /etc/service/$service
     file_link /etc/service/$service /etc/nsd/$service
 }
+
+proc muppet::naviserver_defaults {} {
+    return {#
+# These variables can be customized to change main Naviserver settings.
+# More changes can be done by modifying the /etc/naviserver/default.tcl Tcl script.
+# 
+# Note that these variables are read and interpreted by the Tcl script
+# too, so avoid using shell capabilities in setting vars.
+#
+
+#USER=www-data
+#GROUP=www-data
+# When AUTOSTART is set to all, using DAEMONTOOLS, all instances in DAEMONTOOLS_SVCDIR
+# will be started/stopped/restarted, otherwise all /etc/naviserver/*.tcl config files will
+# be started.
+#AUTOSTART=all
+#AUTOSTART="instance1 instance2"
+#AUTOSTART=none
+
+# IMPORTANT: If using Daemontools to control the Naviserver daemon, remove the init script links
+# to the package script using "update-rc.d -f naviserver remove"
+DAEMONTOOLS=yes
+DAEMONTOOLS_SVCDIR=/etc/service
+DAEMONTOOLS_SVC=/usr/bin/svc
+# If set, the init script assumes the following commands are configured to work correctly:
+# svc -u /instance"
+# svc -t /instance"
+# svc -d /instance"
+# Using allows for non-standard configurations to be specificied in the DAEMONTOOLS_SVCDIR
+# run script for the instance.
+}
+}
+
