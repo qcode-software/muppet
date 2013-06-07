5a4096356541732d3c3297a7d7a05be93b738950
Peter Chaplin <peter@qcode.co.uk>
2013-06-07 10:58:32 +0100

    Allow no config for self-signed cert generation

--- tcl/ssl.tcl
+++ tcl/ssl.tcl
@@ -13,12 +13,16 @@ proc muppet::ssl_key {} {
 proc muppet::ssl_cert {key} {
     install openssl
     package require fileutil
-    set config_filename [file_temp [qc::param ssl_default_cnf]]
     set key_filename [file_temp $key]
     set cert_filename [fileutil::tempfile]
-    sh openssl req -config $config_filename -new -x509 -key $key_filename -days 365 -out $cert_filename
+    if { [qc::param_exists ssl_default_cnf] } {
+        set config_filename [file_temp [qc::param ssl_default_cnf]]
+        sh openssl req -config $config_filename -new -x509 -key $key_filename -days 365 -out $cert_filename
+        file delete $config_filename
+    } else {
+        sh openssl req -new -x509 -key $key_filename -days 365 -out $cert_filename
+    }
     set cert [cat $cert_filename]
-    file delete $config_filename
     file delete $key_filename
     file delete $cert_filename
     return $cert
