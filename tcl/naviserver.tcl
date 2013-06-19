969c95e8d66e8b4e2b7021edb0adffbf2127d6c5
David Osborne <david@qcode.co.uk>
2013-06-19 14:39:08 +0100

    Updated navisever install

--- tcl/naviserver.tcl
+++ tcl/naviserver.tcl
@@ -4,17 +4,23 @@ namespace eval muppet {
     namespace export *
 } 
 
+proc muppet::naviserver_install {} {
+    file_write /etc/default/naviserver [muppet::naviserver_defaults]
+    sh update-rc.d -f naviserver remove
+    user_add nsd
+    file mkdir /var/log/naviserver/
+    file attributes /var/log/naviserver/ -owner nsd -group nsd -permissions 0770
+    file mkdir /var/run/naviserver/
+    file attributes /var/run/naviserver/ -owner nsd -group nsd -permissions 0770
+    install naviserver naviserver-core naviserver-nsdbpg daemontools daemontools-run
+}
+
 proc muppet::naviserver_daemontools_run { service } {
-    #| Naviserver start script which will fall-back to aolserver if Naviserver is not where expected
+    #| Naviserver start script 
     return [subst -nocommands {#!/bin/sh
 export LANG=en_GB.UTF-8
 export ENVIRONMENT=`grep "ENVIRONMENT" /etc/profile | sed "s;.*= *;;"`
-if [ -f /usr/lib/naviserver/bin/nsd ];
-then
 NSD_EXE=/usr/lib/naviserver/bin/nsd
-else
-NSD_EXE=/usr/lib/aolserver4/bin/nsd
-fi
 exec \$NSD_EXE -u nsd -g nsd -i -t /home/nsd/${service}/etc/${service}.tcl 2>&1
 }]
 }
