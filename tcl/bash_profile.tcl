16e56da5783a3b60024a0c91fea70270f248675d
David Osborne <david@qcode.co.uk>
2014-06-12 10:49:05 +0100

    Move proxy env vars to profile

--- tcl/bash_profile.tcl
+++ tcl/bash_profile.tcl
@@ -2,12 +2,21 @@ package provide muppet 1.2.4
 package require qcode
 namespace eval muppet {}
 
-proc muppet::bash_profile {environment} {
+proc muppet::bash_profile {args} {
+    qc::args $args -proxy "" -- environment
     set profile "# /etc/profile: system-wide .profile file for the Bourne shell (sh(1))
 # and Bourne compatible shells (bash(1), ksh(1), ash(1), ...).
 	
 export ENVIRONMENT=$environment
 "
+    if {$proxy ne ""} {
+        set proxy_clause [list "export http_proxy=$proxy"]
+        lappend proxy_clause "export https_proxy=$proxy"
+        lappend proxy_clause ""
+    } else {
+        set proxy_clause ""
+    }
+    append profile [join $proxy_clause \n]
     append profile {if [ "`id -u`" -eq 0 ]; then
   PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 else
@@ -51,7 +60,8 @@ export TMOUT
     return $profile
 }
 
-proc muppet::bash_profile_install {} {
+proc muppet::bash_profile_install {args} {
+    qc::args $args -proxy "" --
     global env
     if { ![info exists env(ENVIRONMENT)] } { 
 	puts "Please Enter The Environment To Be Installed {DEV TESTING STAGING LIVE}:"
@@ -63,6 +73,6 @@ proc muppet::bash_profile_install {} {
 	set env(ENVIRONMENT) $input
     }
        
-    file_write /etc/profile [bash_profile $env(ENVIRONMENT)] 0644
+    file_write /etc/profile [bash_profile -proxy $proxy $env(ENVIRONMENT)] 0644
     puts "installed /etc/profile"
 }
