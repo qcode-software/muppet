07aef999fede3e1dc8b2fbc4d712f3717b3b1abd
Daniel Clark <daniel@qcode.co.uk>
2015-02-06 14:25:42 +0000

    use new muppet procs

--- tcl/cron.tcl
+++ tcl/cron.tcl
@@ -16,23 +16,3 @@ proc muppet::crontab_delete {user} {
     sh crontab -u $user -r
 }
 
-proc muppet::crontab_office_hours {} {
-    #| Returns a crontab which will run during office hours.
-    #| Use for anything that's not a 24/7 server.
-    return {# /etc/crontab: system-wide crontab
-# Unlike any other crontab you don't have to run the `crontab'
-# command to install the new version when you edit this file
-# and files in /etc/cron.d. These files also have username fields,
-# that none of the other crontabs do.
-
-SHELL=/bin/sh
-PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-
-# m h dom mon dow user	command
-17 *	* * *	root    cd / && run-parts --report /etc/cron.hourly
-25 8	* * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.daily )
-47 8	* * 7	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.weekly )
-52 8	1 * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.monthly )
-#
-}
-}
